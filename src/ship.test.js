bbf8fc9566b728adec4c60987fc7d06b21a59989
Alexander Hed <alexander.hed@gmail.com>
2022-04-24 12:09:23 +0200

    changed location of files

---
+++ src/ship.test.js
@@ -0,0 +1,30 @@
+import {Ship} from './ship';
+
+const testShipSmall = Ship(2)
+
+test ('add a hit to the ship', () => {
+    testShipSmall.hit(0);
+});
+
+test ('hit same spot again', () => {
+    testShipSmall.hit(0);
+});
+
+test ('shoud not be sunken', () => {
+    expect(testShipSmall.isSunk()).toBe(false);
+});
+
+test ('hit another spot', () => {
+    testShipSmall.hit(1);
+});
+
+test('should be sunken', () => {
+    expect(testShipSmall.isSunk()).toBe(true);
+});
+
+test ('hit outside length', () => {     // should throw error
+    expect(() => {
+        testShipSmall.hit(2);
+    }).toThrow();
+});
+
