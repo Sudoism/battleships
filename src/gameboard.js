858f4094367b81f804efea3a9a3e2624071f3eb3
Alexander Hed <alexander.hed@gmail.com>
2022-04-25 11:25:36 +0200

    added a function for placing ships using shipfactory

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,12 +1,60 @@
-const gameBoard = (horisontalSize,verticalSize) => {
-    const board = [];
-    for (let i = 0; i < verticalSize; i++) {
-        board[i] = [];
-        for (let j = 0; j < horisontalSize; j++) {
-        board[i][j] = 0;
+import { Ship } from "./ship";
+
+const gameBoard = () => {
+    let grid = Array(10).fill(Array(10).fill(0)); //create a 10x10 grid filled with 0s
+    let ships = [];
+
+    const placeShip = (xCoordinate, yCoordinate, length, direction) => {
+
+        // Error handling of size and direction 
+        if (direction === 'horizontal') {
+            if (xCoordinate + length > 10) {
+                throw new Error('Ship is out of bounds');
+            }
+            for (let i = 0; i < length; i++) {
+                if (grid[xCoordinate + i][yCoordinate] !== 0) {
+                    throw new Error('Ship cannot be placed there');
+                }
+            }
+            for (let i = 0; i < length; i++) {
+                grid[xCoordinate + i][yCoordinate] = 1;
+            }
+        } else if (direction === 'vertical') {
+            if (yCoordinate + length > 10) {
+                throw new Error('Ship is out of bounds');
+            }
+            for (let i = 0; i < length; i++) {
+                if (grid[xCoordinate][yCoordinate + i] !== 0) {
+                    throw new Error('Ship cannot be placed there');
+                }
+            }
+            for (let i = 0; i < length; i++) {
+                grid[xCoordinate][yCoordinate + i] = 1;
+            }
+        } else {
+            throw new Error('Invalid direction');
         }
+
+        // Create a ship object and add it to the ships array
+        const ship = Ship(length);
+        ships.push(ship);
+        let boardShipId = ships.length;
+        grid[xCoordinate][yCoordinate] = boardShipId;
+        if (direction === 'horizontal') {
+            for (let i = 0; i < length; i++) {
+                grid[xCoordinate + i][yCoordinate] = boardShipId;
+            }
+        } else if (direction === 'vertical') {   // if direction is vertical
+            for (let i = 0; i < length; i++) {
+                grid[xCoordinate][yCoordinate + i] = boardShipId;
+            }
+        }
+        return true;
     }
-    return board;
+
+    return {
+        placeShip
+    }
 
 };
 
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -2,6 +2,32 @@ import {gameBoard} from '../src/gameboard.js';
 
 const testBoard = gameBoard();
 
-test ('place a small ship on the board', () => {
-    expect(testBoard.placeShip(0, 0, 2, 'horizontal')).toPass();
+test ('place a small ship horizontal on the board', () => {
+    expect(testBoard.placeShip(0, 0, 2, 'horizontal')).toBe(true);
+});
+
+test ('place a small ship vertical on the board', () => {
+    expect(testBoard.placeShip(1, 1, 2, 'vertical')).toBe(true);
+});
+
+test ('place a small ship vertical where already placed a ship on the board', () => {
+    expect(() => {
+        testBoard.placeShip(1, 1, 2, 'vertical');
+    }).toThrow();
+});
+
+test ('place a small ship horizontal board', () => {
+    expect(() => {
+        testBoard.placeShip(11, 11, 2, 'horizontal');
+    }).toThrow();
+});
+
+test ('place a small ship with invalid direction', () => {
+    expect(() => {
+        testBoard.placeShip(0, 0, 2, 'invalid');
+    }).toThrow();
+});
+
+test ('place a large ship horizontal', () => {
+    expect(testBoard.placeShip(5, 5, 5, 'horizontal')).toBe(true);
 });
