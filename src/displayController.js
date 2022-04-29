335ed487dfabdf7d4a0a30fc576c51b09dc1308b
Alexander Hed <alexander.hed@gmail.com>
2022-04-29 21:05:24 +0200

    able to render 2 boards

--- src/displayController.js
+++ src/displayController.js
@@ -5,8 +5,26 @@ const displayController = () => {
         body.appendChild(document.createElement("h1")).textContent = "Hello World again";
     };
 
+    const displayBoard = (board) => {
+        const table = document.createElement("table");
+        table.classList.add("board");
+        const tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+        for (let i = 0; i < board.length; i++) {
+            const tr = document.createElement("tr");
+            tbody.appendChild(tr);
+            for (let j = 0; j < board[i].length; j++) {
+                const td = document.createElement("td");
+                td.textContent = board[i][j];
+                tr.appendChild(td);
+            }
+        }
+        body.appendChild(table);
+    };
+
     return {
-        helloWorld
+        helloWorld,
+        displayBoard,
     };
 }
 
